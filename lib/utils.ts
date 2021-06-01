db1d070e261e9202dba9503e41dc457320711245
Takuya Shizukuishi <sititou70@gmail.com>
2021-06-01 16:15:16 +0900

    perf: cache RegExp

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,18 @@
 import localeCode from 'locale-code';
 import osLocale from 'os-locale';
 
-export const testString = (str: string, pattern: string): boolean =>
-  new RegExp(pattern).test(str);
+export const testString = (() => {
+  const regExpCache: Map<string, RegExp> = new Map();
+
+  return (str: string, pattern: string): boolean => {
+    const cache = regExpCache.get(pattern);
+    if (cache !== undefined) return cache.test(str);
+
+    const newRegExp = new RegExp(pattern);
+    regExpCache.set(pattern, newRegExp);
+    return newRegExp.test(str);
+  };
+})();
 
 export type LocaleObject<T> = { en: T; [key: string]: T };
 export const localize = <T>(obj: LocaleObject<T>): T => {
